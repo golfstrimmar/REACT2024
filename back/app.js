5310b65ff617c46a24ee8fbdd29410b1086911ab
golfstrimmar <yushinbox@gmail.com>
2024-11-22 09:51:01 +0100

    vercel

--- back/app.js
+++ back/app.js
@@ -14,7 +14,7 @@ app.use(
     // origin: "https://blog-woad-ten-78.vercel.app",
     // origin: "http://localhost:3000",
     // origin: "https://react-blog-beryl-nu.vercel.app",
-    origin: "https://react-blog-git-blog1-golfstrimmars-projects.vercel.app",
+    origin: "https://react-blog-beryl-nu.vercel.app",
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
