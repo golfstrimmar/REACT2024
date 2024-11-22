fb2de0ee38bd182cd53618f72991ab132155334a
golfstrimmar <yushinbox@gmail.com>
2024-11-22 10:17:30 +0100

    vercel

--- back/app.js
+++ back/app.js
@@ -11,10 +11,8 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    // origin: "https://blog-woad-ten-78.vercel.app",
-    origin: "http://localhost:3000",
-    // origin: "https://react-blog-beryl-nu.vercel.app",
-    // origin: "https://react-blog-beryl-nu.vercel.app",
+    // origin: "http://localhost:3000",
+    origin: "https://react-blog-beryl-nu.vercel.app",
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
